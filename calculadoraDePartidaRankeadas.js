82c165c4d1d5069d19d6f93d3ea8544935b72730
Léo Marques <nivmizzeth@gmail.com>
2026-01-05 00:00:10 -0300

    adicionado constantes

--- calculadoraDePartidaRankeadas.js
+++ calculadoraDePartidaRankeadas.js
@@ -1,38 +1,49 @@
 //Calculadora de Partidas Rankeadas
 //Segundo desafio prof Felipe Aguiar
 
-saldoRankeadas(14, 10)
-
 function saldoRankeadas(vitorias, derrotas){
-    let saldoVitorias = vitorias - derrotas
-    return saldoVitorias
-}
-
-nivel(saldoVitorias)
-
-function nivel(classe){
-    let userClasse = ""
     
-    if (classe <= 10 ){
-        userClasse = "Ferro"
-    } else if (classe <= 20){
-        userClasse = "Bronze"
-    } else if (classe <= 50){
-        userClasse = "Prata"
-    } else if (classe <= 80){
-        userClasse = "Ouro"
-    } else if (classe <= 90){
-        userClasse = "Diamante"
-    } else if (classe <= 100){
-        userClasse = "Lendário"
-    } else {
-        userClasse = "Imortal"
-    }
-
-    return userClasse
-
+    return vitorias - derrotas
 }
 
+function definirNivel(classe){
+    
+    switch (true){
 
+        case FERRO_MAX <= 10: 
+            return "Ferro"
+        
+        case BRONZE_MAX <= 20: 
+            return "Bronze"
 
-console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`)
+        case PRATA_MAX <= 50: 
+            return "Prata"
+
+        case OURO_MAX <= 80: 
+            return "Ouro"
+
+        case DIAMANTE_MAX <= 90: 
+            return "Diamante"
+
+        case lENDARIO_MAX <= 100: 
+            return "Lendário"
+
+        case IMORTAL_MAX >= 101: 
+            return "Lendário"
+        }
+}
+
+//niveis de classe declarados em constantes pq se tratam de regra de negocio.
+const FERRO_MAX = 10
+const BRONZE_MAX = 20
+const PRATA_MAX = 50
+const OURO_MAX = 80
+const DIAMANTE_MAX = 90
+const lENDARIO_MAX = 100
+const IMORTAL_MIN = 101
+const IMORTAL_MAX = Infinity
+
+let contadorVitorias = saldoRankeadas(460, 411)
+let nivelAlcancado = definirNivel(contadorVitorias)
+
+console.log(`O Herói tem saldo de ${contadorVitorias} está no nível de ${nivelAlcancado}`)
