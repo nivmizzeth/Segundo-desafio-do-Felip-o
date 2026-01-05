e0f7875f2dc39c9a0f9b255a5849464cae2f5683
Léo Marques <nivmizzeth@gmail.com>
2026-01-04 22:34:57 -0300

    primeiro codigo

--- calculadoraDePartidaRankeadas.js
+++ calculadoraDePartidaRankeadas.js
@@ -4,14 +4,35 @@
 saldoRankeadas(14, 10)
 
 function saldoRankeadas(vitorias, derrotas){
-    saldoVitorias = vitorias - derrotas
+    let saldoVitorias = vitorias - derrotas
     return saldoVitorias
 }
 
+nivel(saldoVitorias)
+
 function nivel(classe){
+    let userClasse = ""
     
+    if (classe <= 10 ){
+        userClasse = "Ferro"
+    } else if (classe <= 20){
+        userClasse = "Bronze"
+    } else if (classe <= 50){
+        userClasse = "Prata"
+    } else if (classe <= 80){
+        userClasse = "Ouro"
+    } else if (classe <= 90){
+        userClasse = "Diamante"
+    } else if (classe <= 100){
+        userClasse = "Lendário"
+    } else {
+        userClasse = "Imortal"
+    }
+
+    return userClasse
+
 }
 
 
 
-console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de **{nivel}`)
+console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`)
