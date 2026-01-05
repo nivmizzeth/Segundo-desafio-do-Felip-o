e66c0bb1cb3fc4320cd56cb2776d43ace2f3e814
Léo Marques <nivmizzeth@gmail.com>
2026-01-04 21:24:58 -0300

    Faze função do nivel

---
+++ calculadoraDePartidaRankeadas.js
@@ -0,0 +1,17 @@
+//Calculadora de Partidas Rankeadas
+//Segundo desafio prof Felipe Aguiar
+
+saldoRankeadas(14, 10)
+
+function saldoRankeadas(vitorias, derrotas){
+    saldoVitorias = vitorias - derrotas
+    return saldoVitorias
+}
+
+function nivel(classe){
+    
+}
+
+
+
+console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de **{nivel}`)
